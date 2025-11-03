6affbdec769e7146bf4fd25a634691e952f3b712
Sebastian Loeffler | MK Logic <82960948+MKL-SebastianLoeffler@users.noreply.github.com>
2025-11-03 16:17:51 +0100

    Role Type Convenience Extensions
    - Add possibility to get name and abbreviation for a specific Role Type.
    - Make sure that the enum value is a byte.

--- ZWave/Enums/RoleTypes.cs
+++ ZWave/Enums/RoleTypes.cs
@@ -1,9 +1,15 @@
 /// SPDX-License-Identifier: BSD-3-Clause
 /// SPDX-FileCopyrightText: Silicon Laboratories Inc. https://www.silabs.com
 /// SPDX-FileCopyrightText: Z-Wave Alliance https://z-wavealliance.org
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Utils;
+
 namespace ZWave.Enums
 {
-    public enum RoleTypes
+    public enum RoleTypes : byte
     {
         None = 0xFF,
         CONTROLLER_CENTRAL_STATIC = 0x00,
@@ -20,6 +26,41 @@ namespace ZWave.Enums
 
     public static class RoleTypes_Extensions
     {
+        private static readonly ReadOnlyDictionary<RoleTypes, RoleTypesInfo> _roleTypesInfoDict = new ReadOnlyDictionary<RoleTypes, RoleTypesInfo>(
+            new Dictionary<RoleTypes, RoleTypesInfo>()
+            {
+                { RoleTypes.None, new RoleTypesInfo("Unknown", "n/a") },
+                { RoleTypes.CONTROLLER_CENTRAL_STATIC, new RoleTypesInfo("Central Static Controller", "CSC") },
+                { RoleTypes.CONTROLLER_SUB_STATIC, new RoleTypesInfo("Sub Static Controller", "SSC") },
+                { RoleTypes.CONTROLLER_PORTABLE, new RoleTypesInfo("Portable Controller", "PC") },
+                { RoleTypes.CONTROLLER_PORTABLE_REPORTING, new RoleTypesInfo("Reporting Portable Controller", "RPC") },
+                { RoleTypes.END_NODE_PORTABLE, new RoleTypesInfo("Portable End Node", "PEN") },
+                { RoleTypes.END_NODE_ALWAYS_ON, new RoleTypesInfo("Always On End Node", "AOEN") },
+                { RoleTypes.END_NODE_SLEEPING_REPORTING, new RoleTypesInfo("Reporting Sleeping End Node", "RSEN") },
+                { RoleTypes.END_NODE_SLEEPING_LISTENING, new RoleTypesInfo("Listening Sleeping End Node", "LSEN") },
+                { RoleTypes.END_NODE_NETWORK_AWARE, new RoleTypesInfo("Network Aware End Node", "NAEN") },
+                { RoleTypes.END_NODE_WAKE_ON_EVENT, new RoleTypesInfo("Wake On Event End Node", "WOEEN") }
+            }
+        );
+
+        public static string GetName(this RoleTypes roleType)
+        {
+            if (_roleTypesInfoDict.TryGetValue(roleType, out RoleTypesInfo info))
+            {
+                return info.Name;
+            }
+            return _roleTypesInfoDict.FirstOrDefault(i => i.Key == RoleTypes.None).Value.Name;
+        }
+
+        public static string GetAbbreviation(this RoleTypes roleType)
+        {
+            if (_roleTypesInfoDict.TryGetValue(roleType, out RoleTypesInfo info))
+            {
+                return info.Abbreviation;
+            }
+            return _roleTypesInfoDict.FirstOrDefault(i => i.Key == RoleTypes.None).Value.Abbreviation;
+        }
+
         /// <summary>
         /// Determines if the Role Type is a controller.
         /// </summary>
@@ -87,6 +128,18 @@ namespace ZWave.Enums
                 default:
                     return false;
             }
+        }
+
+        public class RoleTypesInfo
+        {
+            public RoleTypesInfo(string name, string abbreviation)
+            {
+                Name = name;
+                Abbreviation = abbreviation;
+            }
+
+            public string Name { get; }
+            public string Abbreviation { get; }
         }
     }
 }
