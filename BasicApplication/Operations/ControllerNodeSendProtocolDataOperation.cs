0fe8e954c2e10138bb4756fb711069075b3cba64
Sebastian Loeffler <sebastian.loeffler@mk-logic.de>
2026-02-04 16:39:20 +0100

    Add serial API commands for NLS encryption handling:
    - RequestProtocolCcEncryption
    - ControllerNodeSendProtocolData
    - TransferProtocolCc

---
+++ BasicApplication/Operations/ControllerNodeSendProtocolDataOperation.cs
@@ -0,0 +1,182 @@
+/// SPDX-License-Identifier: BSD-3-Clause
+/// SPDX-FileCopyrightText: Z-Wave Alliance https://z-wavealliance.org
+using System;
+using ZWave.BasicApplication.Enums;
+using ZWave.Devices;
+using ZWave.Enums;
+using Utils;
+using System.Linq;
+
+namespace ZWave.BasicApplication.Operations
+{
+    /// <summary>
+    /// Sends encrypted protocol (Network Layer) data to the module for RF transmission (0xAC).
+    /// HOST->ZW: REQ | 0xAC | destination_node_id | data_length | data[] | protocol_metadata_length | protocol_metadata[] | session_id
+    /// ZW->HOST: RES | 0xAC | RetVal
+    /// ZW->HOST: REQ | 0xAC | funcID | txStatus | ... (same callback as Send Data)
+    /// Used for NLS; only call after receiving <see cref="RequestProtocolCcEncryptionOperation"/> and encrypting the payload.
+    /// </summary>
+    public class ControllerNodeSendProtocolDataOperation : CallbackApiOperation
+    {
+        public NodeTag DestinationNodeId { get; set; }
+        public byte[] Data { get; set; }
+        public byte[] ProtocolMetadata { get; set; }
+        public byte SessionId { get; set; }
+
+        public ControllerNodeSendProtocolDataOperation(NetworkViewPoint network, NodeTag destinationNodeId, byte[] encryptedData, byte[] protocolMetadata, byte sessionId)
+            : base(CommandTypes.CmdZWaveControllerNodeSendProtocolData)
+        {
+            _network = network;
+            DestinationNodeId = destinationNodeId;
+            Data = encryptedData ?? Array.Empty<byte>();
+            ProtocolMetadata = protocolMetadata ?? Array.Empty<byte>();
+            SessionId = sessionId;
+        }
+
+        protected override void CreateInstance()
+        {
+            base.CreateInstance();
+            TimeoutMs = SubstituteSettings.CallbackWaitTimeoutMs;
+            SpecificResult.TransmitStatus = TransmitStatuses.ResMissing;
+        }
+
+        protected override byte[] CreateInputParameters()
+        {
+            int metaLen = ProtocolMetadata?.Length ?? 0;
+            byte[] ret;
+            if (_network.IsNodeIdBaseTypeLR)
+            {
+                ret = new byte[4 + Data.Length + 1 + metaLen + 1];
+                ret[0] = (byte)(DestinationNodeId.Id >> 8);
+                ret[1] = (byte)DestinationNodeId.Id;
+                ret[2] = (byte)Data.Length;
+                Buffer.BlockCopy(Data, 0, ret, 3, Data.Length);
+                ret[3 + Data.Length] = (byte)metaLen;
+                if (metaLen > 0)
+                    Buffer.BlockCopy(ProtocolMetadata, 0, ret, 4 + Data.Length, metaLen);
+                ret[4 + Data.Length + metaLen] = SessionId;
+            }
+            else
+            {
+                ret = new byte[3 + Data.Length + 1 + metaLen + 1];
+                ret[0] = (byte)DestinationNodeId.Id;
+                ret[1] = (byte)Data.Length;
+                Buffer.BlockCopy(Data, 0, ret, 2, Data.Length);
+                ret[2 + Data.Length] = (byte)metaLen;
+                if (metaLen > 0)
+                    Buffer.BlockCopy(ProtocolMetadata, 0, ret, 3 + Data.Length, metaLen);
+                ret[3 + Data.Length + metaLen] = SessionId;
+            }
+            return ret;
+        }
+
+        protected override void OnCallbackInternal(DataReceivedUnit ou)
+        {
+            var payload = ou.DataFrame.Payload;
+            if (payload != null && payload.Length > 1)
+            {
+                SpecificResult.FuncId = payload[0];
+                SpecificResult.TransmitStatus = (TransmitStatuses)payload[1];
+                int index = 3;
+                if (payload.Length > index)
+                {
+                    SpecificResult.HasTxTransmitReport = true;
+                    SpecificResult.TransmitTicks = (ushort)((payload[index - 1] << 8) + payload[index]);
+                    index++;
+                    if (payload.Length > index)
+                    {
+                        SpecificResult.RepeatersCount = payload[index];
+                        index += 5;
+                        if (payload.Length > index)
+                        {
+                            SpecificResult.RssiValuesIncoming = new sbyte[] { (sbyte)payload[index - 4], (sbyte)payload[index - 3], (sbyte)payload[index - 2], (sbyte)payload[index - 1], (sbyte)payload[index] };
+                            index++;
+                            if (payload.Length > index)
+                            {
+                                SpecificResult.AckChannelNo = payload[index];
+                                index++;
+                                if (payload.Length > index)
+                                {
+                                    SpecificResult.LastTxChannelNo = payload[index];
+                                    index++;
+                                    if (payload.Length > index)
+                                    {
+                                        SpecificResult.RouteScheme = (RoutingSchemes)payload[index];
+                                        index += 4;
+                                        if (payload.Length > index)
+                                        {
+                                            SpecificResult.Repeaters = new byte[]
+                                            {
+                                                payload[index - 3],
+                                                payload[index - 2],
+                                                payload[index - 1],
+                                                payload[index]
+                                            };
+                                            index++;
+                                            if (payload.Length > index)
+                                            {
+                                                SpecificResult.RouteSpeed = payload[index];
+                                                index++;
+                                                if (payload.Length > index)
+                                                {
+                                                    SpecificResult.RouteTries = payload[index];
+                                                    index++;
+                                                    if (payload.Length > index)
+                                                    {
+                                                        SpecificResult.LastFailedLinkFrom = payload[index];
+                                                        index++;
+                                                        if (payload.Length > index)
+                                                        {
+                                                            SpecificResult.LastFailedLinkTo = payload[index];
+                                                            index++;
+                                                            if (payload.Length > index)
+                                                            {
+                                                                SpecificResult.UsedTxpower = (sbyte)payload[index];
+                                                                index++;
+                                                                if (payload.Length > index)
+                                                                {
+                                                                    SpecificResult.MeasuredNoiseFloor = (sbyte)payload[index];
+                                                                    index++;
+                                                                    if (payload.Length > index)
+                                                                    {
+                                                                        SpecificResult.AckDestinationUsedTxPower = (sbyte)payload[index];
+                                                                        index++;
+                                                                        if (payload.Length > index)
+                                                                        {
+                                                                            SpecificResult.DestinationAckMeasuredRSSI = (sbyte)payload[index];
+                                                                            index++;
+                                                                            if (payload.Length > index)
+                                                                            {
+                                                                                SpecificResult.DestinationckMeasuredNoiseFloor = (sbyte)payload[index];
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public override string AboutMe()
+        {
+            return $"Data={Data?.GetHex()}; SessionId={SessionId}; {SpecificResult.GetReport()}";
+        }
+
+        public SendDataResult SpecificResult => (SendDataResult)Result;
+
+        protected override ActionResult CreateOperationResult()
+        {
+            return new SendDataResult();
+        }
+    }
+}
---
+++ BasicApplication/Operations/RequestProtocolCcEncryptionCallbackOperation.cs
@@ -0,0 +1,92 @@
+/// SPDX-License-Identifier: BSD-3-Clause
+/// SPDX-FileCopyrightText: Z-Wave Alliance https://z-wavealliance.org
+using System;
+using ZWave.BasicApplication.Enums;
+using ZWave.Enums;
+
+namespace ZWave.BasicApplication.Operations
+{
+    /// <summary>
+    /// Sends the TX report back to the module for a prior Request Protocol CC Encryption (0x6C) flow.
+    /// HOST->ZW: REQ | 0x6C | session_id | tx_status | tx_report...
+    /// Call after <see cref="ControllerNodeSendProtocolDataOperation"/> completes to report transmit result.
+    /// </summary>
+    public class RequestProtocolCcEncryptionCallbackOperation : ApiOperation
+    {
+        public byte SessionId { get; set; }
+        public byte TxStatus { get; set; }
+        public SendDataResult TxReport { get; set; }
+
+        public RequestProtocolCcEncryptionCallbackOperation(byte sessionId, byte txStatus, SendDataResult txReport)
+            : base(false, CommandTypes.CmdZWaveRequestProtocolCcEncryption, false)
+        {
+            SessionId = sessionId;
+            TxStatus = txStatus;
+            TxReport = txReport;
+        }
+
+        private ApiMessage _message;
+
+        protected override void CreateWorkflow()
+        {
+            ActionUnits.Add(new StartActionUnit(SetStateCompleting, 0, _message));
+        }
+
+        protected override void CreateInstance()
+        {
+            _message = new ApiMessage(SerialApiCommands[0], CreateCallbackPayload());
+            _message.IsSequenceNumberRequired = false;
+        }
+
+        private byte[] CreateCallbackPayload()
+        {
+            var r = TxReport;
+            byte[] rep = r?.Repeaters;
+            byte rep0 = (rep != null && rep.Length > 0) ? rep[0] : (byte)0;
+            byte rep1 = (rep != null && rep.Length > 1) ? rep[1] : (byte)0;
+            byte rep2 = (rep != null && rep.Length > 2) ? rep[2] : (byte)0;
+            byte rep3 = (rep != null && rep.Length > 3) ? rep[3] : (byte)0;
+            sbyte[] rssi = r?.RssiValuesIncoming;
+            sbyte rssi0 = (rssi != null && rssi.Length > 0) ? rssi[0] : (sbyte)0;
+            sbyte rssi1 = (rssi != null && rssi.Length > 1) ? rssi[1] : (sbyte)0;
+            sbyte rssi2 = (rssi != null && rssi.Length > 2) ? rssi[2] : (sbyte)0;
+            sbyte rssi3 = (rssi != null && rssi.Length > 3) ? rssi[3] : (sbyte)0;
+            // C: beam_1000ms (bit6), beam_250ms (bit5), last_route_speed (bits 0-2)
+            byte beamSpeed = (byte)((r != null ? (int)r.RouteSpeed & 0x07 : 0));
+            return new byte[]
+            {
+                SessionId,
+                TxStatus,
+                (byte)((r?.TransmitTicks ?? 0) >> 8),
+                (byte)((r?.TransmitTicks ?? 0) & 0xFF),
+                r?.RepeatersCount ?? 0,
+                0, // ack_rssi (not stored in SendDataResult)
+                (byte)rssi0,
+                (byte)rssi1,
+                (byte)rssi2,
+                (byte)rssi3,
+                r?.AckChannelNo ?? 0,
+                r?.LastTxChannelNo ?? 0,
+                (byte)(r?.RouteScheme ?? 0),
+                rep0,
+                rep1,
+                rep2,
+                rep3,
+                beamSpeed,
+                r?.RouteTries ?? 0,
+                r?.LastFailedLinkFrom ?? 0,
+                r?.LastFailedLinkTo ?? 0,
+                (byte)(r?.UsedTxpower ?? 0),
+                (byte)(r?.MeasuredNoiseFloor ?? 0),
+                (byte)(r?.AckDestinationUsedTxPower ?? 0),
+                (byte)(r?.DestinationAckMeasuredRSSI ?? 0),
+                (byte)(r?.DestinationckMeasuredNoiseFloor ?? 0),
+            };
+        }
+
+        protected override ActionResult CreateOperationResult()
+        {
+            return new ActionResult();
+        }
+    }
+}
